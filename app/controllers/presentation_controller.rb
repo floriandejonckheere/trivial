8afb87891f0523b585262945de325ca6300d38b0
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-03-13 17:47:43 +0100

    Show only categories that contain cards

--- app/controllers/presentation_controller.rb
+++ app/controllers/presentation_controller.rb
@@ -5,6 +5,7 @@ class PresentationController < ApplicationController
   def show
     @cardset = Cardset.find(params[:cardset_id])
     @category = Category.find(params[:category_id])
+    @categories = Category.find(@cardset.cards.pluck(:category_id).uniq)
     respond_to :html
   end
 
