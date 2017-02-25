6aa76c3daba639820ae79ded4b50dc0ce551bf6f
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-02-25 21:40:35 +0100

    Add French locale

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Trivial
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    I18n.config.available_locales = :fr
+    I18n.default_locale = 'fr'
   end
 end
