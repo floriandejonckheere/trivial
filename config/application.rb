68c2b91f0030d6860f4fe2c2ffc4b1d5df6d2a4e
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-11-25 22:50:38 +0100

    Update binstubs and application framework code

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Trivial
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -19,13 +22,5 @@ module Trivial
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
-    # Make belongs_to associations required
-    config.active_record.belongs_to_required_by_default = true
-
-    ActiveSupport.halt_callback_chains_on_return_false = false
   end
 end
