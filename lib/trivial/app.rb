da20f735787e69af6dd9877fa2fbf4c00dc85e64
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-10-28 19:24:14 +0100

    Add binstubs

---
+++ lib/trivial/app.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Trivial
+  module App
+    NAME = "Trivial"
+  end
+end
---
+++ lib/trivial/version.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Trivial
+  module Version
+    MAJOR = 4
+    MINOR = 0
+    PATCH = 0
+    PRE   = nil
+
+    VERSION = [MAJOR, MINOR, PATCH].compact.join(".")
+
+    STRING = [VERSION, PRE].compact.join("-")
+  end
+
+  VERSION = Trivial::Version::STRING
+end
