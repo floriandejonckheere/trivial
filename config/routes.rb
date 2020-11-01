e294f67ab5ae0f883c2632fd44706ff9e40ca960
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-11-01 18:19:07 +0100

    Make columns broader

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :cardsets do
     resources :cards, except: [:index, :show]
 
-    get "/presentation", controller: "presentation", action: "show"
+    get :presentation, to: "presentation#show"
   end
 
   resources :categories, except: :show
